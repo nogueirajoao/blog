629c64fff2e93bed983ad615978a757953459e71
Joao Nogueira <nogueirajoao>
2019-07-03 00:04:04 +0100

    Redux Thunk middleware, reducers and actions

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceHolder from '../apis/jsonplaceholder';
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceHolder.get('/posts');
+    
+    dispatch({ type: 'FETCH_POSTS', response });
+};
