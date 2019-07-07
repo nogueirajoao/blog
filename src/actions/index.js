dba0bce06292e450a1e7304ae8ff961be6376958
Joao Nogueira <nogueirajoao>
2019-07-07 01:38:45 +0100

    Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,24 @@
 import jsonPlaceHolder from '../apis/jsonplaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
     
-    dispatch({ type: 'FETCH_POSTS', response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USERS', payload: response.data });
 };
